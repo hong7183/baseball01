02a658818b70f96a9271db2f45fa9210e014b664
mtfweb <59831218+mtfweb@users.noreply.github.com>
2025-08-20 17:42:14 +0900

    FA선수 정보 창

--- baseball/src/main/java/com/myspring/baseball/controller/baseballControllerImpl.java
+++ baseball/src/main/java/com/myspring/baseball/controller/baseballControllerImpl.java
@@ -32,10 +32,49 @@ public class baseballControllerImpl extends MultiActionController implements bas
 	baseballVO baseballVO;
 
 	@Override
+	@RequestMapping(value="/baseball/listMembers.do" ,method = RequestMethod.GET)
 	public ModelAndView listMembers(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	} 
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("html/text;charset=utf-8");
+		String viewName = getViewName(request);
+		System.out.println("viewName :: "+ viewName );
+		List membersList = memberService.listMembers();
+		ModelAndView mav = new ModelAndView(viewName);
+		mav.addObject("membersList", membersList);
+		return mav;
+	}
+	
+	private String getViewName(HttpServletRequest request) throws Exception {
+		String contextPath = request.getContextPath();
+		String uri = (String) request.getAttribute("javax.servlet.include.request_uri");
+		if (uri == null || uri.trim().equals("")) {
+			uri = request.getRequestURI();
+		}
+
+		int begin = 0;
+		if (!((contextPath == null) || ("".equals(contextPath)))) {
+			begin = contextPath.length();
+		}
+
+		int end;
+		if (uri.indexOf(";") != -1) {
+			end = uri.indexOf(";");
+		} else if (uri.indexOf("?") != -1) {
+			end = uri.indexOf("?");
+		} else {
+			end = uri.length();
+		}
+
+		String viewName = uri.substring(begin, end);
+		if (viewName.indexOf(".") != -1) {
+			viewName = viewName.substring(0, viewName.lastIndexOf("."));
+		}
+		if (viewName.lastIndexOf("/") != -1) {
+			viewName = viewName.substring(viewName.lastIndexOf("/", 1), viewName.length());
+		}
+		return viewName;
+	}
+	
 	
 //	
 //	public void setMemberService(MemberService memberService) {
@@ -43,7 +82,7 @@ public class baseballControllerImpl extends MultiActionController implements bas
 //	}
 	
 	/*
-	 * // �˻�
+	 * // 占싯삼옙
 	 * 
 	 * @RequestMapping(value="/member/searchMemberList.do", method=
 	 * {RequestMethod.GET, RequestMethod.POST}) public ModelAndView
@@ -67,8 +106,8 @@ public class baseballControllerImpl extends MultiActionController implements bas
 	 * request, HttpServletResponse response) throws Exception{
 	 * 
 	 * String viewName = getViewName(request);
-	 * logger.info("info 레벨: viewName = "+viewName);
-	 * logger.debug("debug 레벨: viewName = "+viewName); List membersList =
+	 * logger.info("info �젅踰�: viewName = "+viewName);
+	 * logger.debug("debug �젅踰�: viewName = "+viewName); List membersList =
 	 * memberService.listMembers(); ModelAndView mav = new ModelAndView(viewName);
 	 * mav.addObject("membersList", membersList);
 	 * 
@@ -102,11 +141,11 @@ public class baseballControllerImpl extends MultiActionController implements bas
 	 * if(memberVO != null) {
 	 * 
 	 * HttpSession session =request.getSession();
-	 * session.setAttribute("member",memberVO); // 로그인 정보를 활용하기 위해 사용
-	 * session.setAttribute("isLogOn",true); // 로그인 유무를 체크 시 사용
+	 * session.setAttribute("member",memberVO); // 濡쒓렇�씤 �젙蹂대�� �솢�슜�븯湲� �쐞�빐 �궗�슜
+	 * session.setAttribute("isLogOn",true); // 濡쒓렇�씤 �쑀臾대�� 泥댄겕 �떆 �궗�슜
 	 * 
 	 * mav.setViewName("redirect:/member/listMembers.do"); }else {
-	 * logger.info("null 입니다."); mav.setViewName("redirect:/member/loginForm.do"); }
+	 * logger.info("null �엯�땲�떎."); mav.setViewName("redirect:/member/loginForm.do"); }
 	 * return mav; }
 	 * 
 	 * 
@@ -160,11 +199,11 @@ public class baseballControllerImpl extends MultiActionController implements bas
 	 * request.setCharacterEncoding("utf-8");
 	 * response.setContentType("text/html; charset=utf-8");
 	 * 
-	 * // 1. request �����͸� MemberVO�� ��´�. //MemberVO memberVO = new MemberVO();
-	 * //bind(request, memberVO); // 2. memberService�� �޼��带 �߰� �Ѵ�.
+	 * // 1. request 占쏙옙占쏙옙占싶몌옙 MemberVO占쏙옙 占쏙옙쨈占�. //MemberVO memberVO = new MemberVO();
+	 * //bind(request, memberVO); // 2. memberService占쏙옙 占쌨쇽옙占썲를 占쌩곤옙 占싼댐옙.
 	 * memberService.updateMember(memberVO);
 	 * 
-	 * // 3. ����Ʈ �������� �̵� ModelAndView mav = new
+	 * // 3. 占쏙옙占쏙옙트 占쏙옙占쏙옙占쏙옙占쏙옙 占싱듸옙 ModelAndView mav = new
 	 * ModelAndView("redirect:/member/listMembers.do");
 	 * 
 	 * return mav; }
@@ -177,12 +216,12 @@ public class baseballControllerImpl extends MultiActionController implements bas
 	 * request.setCharacterEncoding("utf-8");
 	 * response.setContentType("text/html; charset=utf-8");
 	 * 
-	 * // 1. request �����´�. // String id = request.getParameter("id");
+	 * // 1. request 占쏙옙占쏙옙占승댐옙. // String id = request.getParameter("id");
 	 * System.out.println("id = "+ id);
 	 * 
-	 * // 2. memberService�� �޼��带 �߰� �Ѵ�. memberService.delMember(id);
+	 * // 2. memberService占쏙옙 占쌨쇽옙占썲를 占쌩곤옙 占싼댐옙. memberService.delMember(id);
 	 * 
-	 * // 3. ����Ʈ �������� �̵� ModelAndView mav = new
+	 * // 3. 占쏙옙占쏙옙트 占쏙옙占쏙옙占쏙옙占쏙옙 占싱듸옙 ModelAndView mav = new
 	 * ModelAndView("redirect:/member/listMembers.do");
 	 * 
 	 * return mav; }
@@ -197,24 +236,24 @@ public class baseballControllerImpl extends MultiActionController implements bas
 	 * request.getAttribute("javax.servlet.include.request_uri"); if (uri == null ||
 	 * uri.trim().equals("")) { uri = request.getRequestURI(); }
 	 * 
-	 * // http://localhost:8090/member/listMember.do�� ��û�� int begin = 0; // if
+	 * // http://localhost:8090/member/listMember.do占쏙옙 占쏙옙청占쏙옙 int begin = 0; // if
 	 * (!((contextPath == null) || ("".equals(contextPath)))) { begin =
-	 * contextPath.length(); // ��ü ��û�� �� ���̸� ���� }
+	 * contextPath.length(); // 占쏙옙체 占쏙옙청占쏙옙 占쏙옙 占쏙옙占싱몌옙 占쏙옙占쏙옙 }
 	 * 
-	 * int end; if (uri.indexOf(";") != -1) { end = uri.indexOf(";"); // ��û uri��
-	 * ';'�� ���� ��� ';'���� ��ġ�� ���� } else if (uri.indexOf("?") != -1) { end =
-	 * uri.indexOf("?"); // ��û uri�� '?'�� ���� ��� '?' ���� ��ġ�� ���� } else {
+	 * int end; if (uri.indexOf(";") != -1) { end = uri.indexOf(";"); // 占쏙옙청 uri占쏙옙
+	 * ';'占쏙옙 占쏙옙占쏙옙 占쏙옙占� ';'占쏙옙占쏙옙 占쏙옙치占쏙옙 占쏙옙占쏙옙 } else if (uri.indexOf("?") != -1) { end =
+	 * uri.indexOf("?"); // 占쏙옙청 uri占쏙옙 '?'占쏙옙 占쏙옙占쏙옙 占쏙옙占� '?' 占쏙옙占쏙옙 占쏙옙치占쏙옙 占쏙옙占쏙옙 } else {
 	 * end = uri.length(); }
 	 * 
-	 * // http://localhost:8090/member/listMember.do�� ��û�� ���� '.do'�� ������ //
-	 * http://localhost:8090/member/listMember�� ���� ��, // �ٽ�
-	 * http://localhost:8090/member/listMember���� �������� ù��° '/' ��ġ�� ���� ��,
-	 * �� ���� // listMember�� ���Ѵ�. String fileName = uri.substring(begin, end);
+	 * // http://localhost:8090/member/listMember.do占쏙옙 占쏙옙청占쏙옙 占쏙옙占쏙옙 '.do'占쏙옙 占쏙옙占쏙옙占쏙옙 //
+	 * http://localhost:8090/member/listMember占쏙옙 占쏙옙占쏙옙 占쏙옙, // 占쌕쏙옙
+	 * http://localhost:8090/member/listMember占쏙옙占쏙옙 占쏙옙占쏙옙占쏙옙占쏙옙 첫占쏙옙째 '/' 占쏙옙치占쏙옙 占쏙옙占쏙옙 占쏙옙,
+	 * 占쏙옙 占쏙옙占쏙옙 // listMember占쏙옙 占쏙옙占싼댐옙. String fileName = uri.substring(begin, end);
 	 * if (fileName.indexOf(".") != -1) { fileName = fileName.substring(0,
-	 * fileName.lastIndexOf(".")); // ��û���� �������� ���� '.'�� ��ġ�� ������,
-	 * '.do' �տ������� ���ڿ��� // ���� } if (fileName.lastIndexOf("/") != -1) {
+	 * fileName.lastIndexOf(".")); // 占쏙옙청占쏙옙占쏙옙 占쏙옙占쏙옙占쏙옙占쏙옙 占쏙옙占쏙옙 '.'占쏙옙 占쏙옙치占쏙옙 占쏙옙占쏙옙占쏙옙,
+	 * '.do' 占쌌울옙占쏙옙占쏙옙占쏙옙 占쏙옙占쌘울옙占쏙옙 // 占쏙옙占쏙옙 } if (fileName.lastIndexOf("/") != -1) {
 	 * fileName = fileName.substring(fileName.lastIndexOf("/"), fileName.length());
-	 * // ��û���� �������� ���� '/'�� ��ġ�� ������, // '/' ���������� ���ڿ��� ���� }
+	 * // 占쏙옙청占쏙옙占쏙옙 占쏙옙占쏙옙占쏙옙占쏙옙 占쏙옙占쏙옙 '/'占쏙옙 占쏙옙치占쏙옙 占쏙옙占쏙옙占쏙옙, // '/' 占쏙옙占쏙옙占쏙옙占쏙옙占쏙옙 占쏙옙占쌘울옙占쏙옙 占쏙옙占쏙옙 }
 	 * return fileName; }
 	 */
 
